6c42cfda5ec2011d72bee70748a8ea4c0669c33e
Maarten Beeckmans <maartenb@inuits.eu>
2020-07-08 13:38:39 +0200

    add installation kubectl

--- provisioning/minikube.sh
+++ provisioning/minikube.sh
@@ -42,6 +42,19 @@ rpm -ivh minikube-latest.x86_64.rpm
 debug "Installing podman"
 yum install -y podman
 
+debug "Adding kubectl repository"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+debug "Intalling kubectl"
+yum -y install kubectl
+
 debug "Installing Helm 3"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 log "Server specific provisioning tasks for ${HOSTNAME} finished"
